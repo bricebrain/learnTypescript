ec93c54342851f93cd591aa201e75fcb25931d9c
BriceAbe <briceabe@gmail.com>
2023-08-02 17:40:51 +0200

    interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
+interface User{
+  name:string,
+  age:number,
+ 
+}
+
+interface UserPlus{
+  name:string,
+  age:number,
+  taille:number
+}
+
+
+type ArrayDeTuple= [number, string] 
+
+type TypeAdd = (...arg:number[])=>number
+
+class UserPerso{
+
+     constructor(public name:string , public age:number){
+     }
+     
+}
+
+
+function description (p:User):void {
+
+}
+ 
+
+
+
 function App() {
+  const lenombre:number = 6 //
+
+  const theo:User =new UserPerso('theo', 7)
+  const [first, setfirst] = useState<User|null >(theo)
+ 
+
+const maFonction = ():void=>{
+
+  setfirst({name:'brice', age: add(1, 2 , 3, 4)})
+  description(theo)
+ 
+}
+
+
+// declaration d array 
+const arr : number[] = [1, 2, 3]
+const arr2:Array<number>= [1, 2]
+
+// declaration tuple
+
+const tup : ArrayDeTuple[] = [[1, 'string'] , [1, 'string']  ]
+
+
+const add:TypeAdd =( ...arg:number[]) =>  {
+  
+  let result:number = 0
+  console.log(result)
+
+  arg.forEach(element => {
+    result += element
+  });
+
+  return result
+}
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 > {first?.name}</h1>
+      <h1 > {first?.age}</h1>
+
+      <button onClick={():void =>maFonction()}> Click</button>
+     
     </div>
   );
 }
